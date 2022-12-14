2f0e30ad41c3452e917f19d9a164ef44982dcc8e
domia2814 <106426622+domia2814@users.noreply.github.com>
2022-12-14 09:55:14 +0100

    warehouse item id refactored, new-warehouse styling

--- src/app/warehouse/warehouse.model.ts
+++ src/app/warehouse/warehouse.model.ts
@@ -3,14 +3,14 @@ export class Warehouse {
   public address: string;
   public capacity: number;
   public enabled: boolean;
-  public id: number;
+  public id: string;
 
-  constructor(location: string, address: string, capacity: number, enabled: boolean, id: number) {
+  constructor(location: string, address: string, capacity: number, enabled: boolean, id: string) {
     this.location = location;
     this.address = address;
     this.capacity = capacity;
     this.enabled = enabled;
-    this.id = Date.now()
+    this.id = `WH_` + Date.now()
   }
 
 }
