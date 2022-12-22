4f5038d4c4ae21d7c4b9ebb68c653b230656604a
Anna Szpecht <aszpecht@sii.pl>
2022-12-22 15:09:53 +0100

    get brands

--- src/app/stores/new-store/new-store.component.ts
+++ src/app/stores/new-store/new-store.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {Form, FormArray, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {Store} from "./store.model";
 import {StoreService} from "./store.service";
@@ -13,12 +13,13 @@ export interface IBrands {
   templateUrl: 'new-store.component.html',
   styleUrls: ['./new-store.component.scss']
 })
-export class NewStoreComponent implements OnInit {
+export class NewStoreComponent implements OnInit, AfterViewInit {
   stores: Store [];
   newStoreForm: FormGroup;
-  Data: Array<any> = [
-    {name: 'Kazar', value: 'Kazar'},
-    {name: 'Gino Rossi', value: 'Gino Rossi'},
+  skillsForm: FormGroup;
+  Data = [
+    {id: 1, name: 'Kazar', value: 'Kazar'},
+    {id: 2, name: 'Gino Rossi', value: 'Gino Rossi'},
     {name: 'Ecco', value: 'Ecco'},
     {name: 'Geox', value: 'Geox'},
     {name: 'Nike', value: 'Nike'},
@@ -28,7 +29,14 @@ export class NewStoreComponent implements OnInit {
     {name: 'Adidas', value: 'Adidas'}
   ];
 
-  constructor(private storeService: StoreService) {
+  constructor(private storeService: StoreService,
+              private cd: ChangeDetectorRef,
+              private fb: FormBuilder) {
+
+    this.skillsForm = this.fb.group({
+      name: '',
+      skills: this.fb.array([]),
+    });
   }
 
   ngOnInit(): void {
@@ -36,8 +44,12 @@ export class NewStoreComponent implements OnInit {
       'location': new FormControl(null, Validators.required),
       'address': new FormControl(null, Validators.required),
       'all': new FormControl(null),
-      'brands': new FormArray([])
-    })
+      'brands': new FormArray([
+        new FormControl('kazar'),
+
+      ])
+    });
+    this.setPreset();
     this.storeService.storesChanged.subscribe(
       (stores: Store[]) => {
         this.stores = stores;
@@ -46,27 +58,60 @@ export class NewStoreComponent implements OnInit {
     console.log(this.storeService.getStores());
   }
 
+  ngAfterViewInit() {
+    this.cd.detectChanges();
+  }
+
+  get brands() {
+    return this.newStoreForm.get('brands') as FormArray;
+  }
+
+  setPreset() {
+    // this.brands.patchValue(["kazar", "Gino Rossi", "ecco", "Geox", "Nike", "Wojas", "Ryłko", "4F", "Adidas"]);
+  }
+
   onSubmit() {
     const newStore = new Store(this.newStoreForm.value['location'], this.newStoreForm.value['address'], this.newStoreForm.value['brands']);
     console.log('new Store ' + newStore.location);
-    this.storeService.addStore(newStore);
-    // this.Data.forEach((check) => check.checked = !check.checked);
-    // this.Data.some((check) => check.checked = !check.checked);
+    console.log('new Store ' + this.brands.value);
+    console.log(this.newStoreForm.get('brands')?.value);
 
+    this.newStoreForm.value['brands']
+    // .map((checked) => checked ? this.Data.values() : null)
+    // .filter((value) => value !== null);
+    this.storeService.addStore(newStore);
     this.newStoreForm.reset();
   }
 
   checkUncheckAll(event: any) {
-    this.Data.forEach((check) => check.checked = event.target.checked);
-    const s = this.Data.map(el => el.name);
-    const brands: FormArray = this.newStoreForm.get('brands') as FormArray;
-    brands.push(new FormControl(s));
-    }
+    // this.Data.forEach((check) => check.checked = event.target.checked);
+    // const s = this.Data.map(el => el.name);
+    // const brands: FormArray = this.newStoreForm.get('brands') as FormArray;
+    // brands.push(new FormControl(s));
+  }
 
-  onCheckboxChange(event: any) {
+  onCheckboxChange(event: any, i: number) {
     const brands: FormArray = this.newStoreForm.get('brands') as FormArray;
     if (event.target.checked) {
       brands.push(new FormControl(event.target.value));
+    }
+    // this.brands.controls.forEach(control => control.value)
+    //   .map((checked) => checked.checked ? checked.value : null)
+    //   .filter(value => value !== null);
+
+    // if (event.target.checked) {
+    //  this.brands.patchValue(event.target.value);
+    // }
+
+
+
+  }
+
+  onSelectAll() {
+    if (this.brands.controls.some(control => control.value)) {
+      this.brands.controls.forEach(control => control.patchValue(this.brands.value));
+    } else {
+      this.brands.controls.forEach(control => control.patchValue(this.brands.value));
     }
   }
 
